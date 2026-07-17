0176175b27f8a5929b9dc5c1919c733fa0dfc29e
akashyadav1284 <akashyadav9992462520@gmail.com>
2026-07-17 17:48:58 +0530

    chore: integrate Google Analytics Tag to user website layout

--- cargex-user/src/app/layout.tsx
+++ cargex-user/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
+import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -54,7 +55,23 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       }}
     >
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          {/* Google Analytics Tag */}
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-C5SCDVXX66"
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-C5SCDVXX66');
+            `}
+          </Script>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
